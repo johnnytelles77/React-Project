2fc5b4a34145b03f5d2205bd2735a45ea6a3aeff
johnnytelles77 <137516577+johnnytelles77@users.noreply.github.com>
2024-02-26 08:49:07 -0500

    Contador arreglado

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,27 +1,22 @@
-
 import { useState } from "react";
 
-export const useCounter = (minValue = 0, maxValue = 1, step = 1) => {
-    const [count, setCount] = useState(minValue);
+export const useCounter = (minValue = 0, maxValue = 10, step = 1) => {
+  const [count, setCount] = useState(minValue);
 
-    const increment = () => {
-        if (count + step <= maxValue) {
-            setCount(count + step);
-            console.log("Incremented. New Count:", count + step);
-        }
-    };
+  const increment = () => {
+    if (count + step <= maxValue) 
+        setCount(count + step)
+  }
 
-    const decrement = () => {
-        if (count - step >= minValue) {
-            setCount(count - step);
-            console.log("Decremented. New Count:", count - step);
-        }
-    };
+  const decrement = () => {
+    if (count - step >= minValue) {
+         setCount(count - step)
+    }
+  }
 
-    const reset = () => {
-        setCount(minValue);
-        console.log("Reset. New Count:", minValue);
-    };
+  const reset = () => {
+    setCount(minValue)
+  }
 
-    return { count, increment, decrement, reset };
-};
+  return { count, increment, decrement, reset }
+}
